98220a2bc32f51b07dbb693da4cf09a80316ccf0
thlamz <thlamenza@hotmail.com>
2022-06-04 00:34:31 -0300

    Creating application

---
+++ index.js
@@ -0,0 +1,84 @@
+const http = require('http')
+const server = http.createServer();
+const { Server } = require("socket.io")
+const { hrtime } =  require("process")
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = new Sequelize("");
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:8000",
+        methods: ["GET", "POST"]
+    },
+});
+
+const Entry = sequelize.define('User', {
+    host: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    latency: {
+      type: DataTypes.DOUBLE,
+      allowNull: false
+    }
+  }, {});
+Entry.sync()
+
+
+async function get_host_latency(host) {
+    let entry = await Entry.findOne({
+        where: {
+            "host": host
+        }
+    })
+    return entry === null ? 0 : entry.latency
+}
+
+function set_host_latency(host, latency) {
+    let entry = await Entry.findOne({
+        where: {
+            "host": host
+        }
+    })
+    if(entry === null) {
+        entry = await Entry.create({
+            host: host,
+            latency: latency
+        })
+    } else {
+        entry.latency = latency
+        await entry.save()
+    }
+    return entry
+}
+
+function register_measure(socket) {
+    let lastMeasure;
+    let lastPing;
+    socket.conn.on("packet", ({data}) => {
+        if(typeof data === 'string' && data.includes("pongMeasure")) {
+            lastMeasure = Number(hrtime.bigint() - lastPing) / 1e6
+            socket.emit("latency", lastMeasure)
+            registered_latencies[socket.id] = lastMeasure
+        }
+    })
+
+    let interval = setInterval(
+        () => {
+            socket.send("pingMeasure")
+            lastPing = hrtime.bigint()
+        }, 500
+    )
+
+    socket.on("disconnect", () => {
+        clearInterval(interval)
+    })
+}
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    register_measure(socket)
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
