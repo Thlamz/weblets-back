4258cee04b18e2a208bb86b8c586bb2b18b74155
thlamz <thlamenza@hotmail.com>
2022-06-13 20:36:38 -0300

    Fixing and making requires more explicit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { Server } = require("socket.io")
 const express = require("express");
 const cors = require("cors");
-const { socket_handlers: ping_socket_handlers, router: ping_router, route: ping_route } = require("./ping");
-const { socket_handlers: get_away_socket_handlers, router: get_away_router, route: get_away_route } = require("./getAway");
+const { socket_handlers: ping_socket_handlers, router: ping_router, route: ping_route } = require("./ping/index.js");
+const { socket_handlers: get_away_socket_handlers, router: get_away_router, route: get_away_route } = require("./go-away/index.js");
 
 const PORT = process.env.PORT || 3000;
 
