09a12bff57e764e68eb594351f5a20cee70fb483
thlamz <thlamenza@hotmail.com>
2022-06-16 17:40:15 -0300

    Fixing go-away route

--- go-away/index.ts
+++ go-away/index.ts
@@ -2,7 +2,7 @@ import { Router } from "express"
 import { get_last_access, register_access, last_day_acess_count } from "./access"
 
 export let router = Router()
-export const route = '/go_away'
+export const route = '/go-away'
 
 router.get('/', async (req, res) => {
     let last_access = await get_last_access()
--- weblets.ts
+++ weblets.ts
@@ -2,7 +2,7 @@ import { Router } from "express"
 import { Server, Socket } from "socket.io"
 
 import { socket_handlers as ping_socket_handlers, router as ping_router, route as ping_route } from"./ping/index";
-import { socket_handlers as get_away_socket_handlers, router as get_away_router, route as get_away_route } from "./go-away/index"
+import { socket_handlers as go_away_socket_handlers, router as go_away_router, route as go_away_route } from "./go-away/index"
 
 export type SocketHandler = (io: Server, socket: Socket) => Promise<void>
 
@@ -11,10 +11,10 @@ export type EndpointRouter = [string, Router]
 
 export const socket_handlers: Array<SocketHandler> = [
     ...ping_socket_handlers,
-    ...get_away_socket_handlers
+    ...go_away_socket_handlers
 ]
 
 export const endpoint_routers: Array<EndpointRouter> = [
     [ping_route, ping_router],
-    [get_away_route, get_away_router]
+    [go_away_route, go_away_router]
 ]
